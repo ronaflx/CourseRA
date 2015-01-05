36f8923b5ce89a40d8897a9245920496187aab74
Lixin Fan <900831flx@gmail.com>
2015-01-05 16:15:33 +0800

    Finish lab1

--- 6.824-golabs-2014/src/main/wc.go
+++ 6.824-golabs-2014/src/main/wc.go
@@ -4,15 +4,32 @@ import "os"
 import "fmt"
 import "mapreduce"
 import "container/list"
+import "strings"
+import "unicode"
+
+func notLetter(r rune) bool {
+	return !unicode.IsLetter(r)
+}
 
 // our simplified version of MapReduce does not supply a
 // key to the Map function, as in the paper; only a value,
 // which is a part of the input file contents
 func Map(value string) *list.List {
+	var words []string = strings.FieldsFunc(value, notLetter)
+	var return_list *list.List = list.New()
+	for _, word := range words {
+		if word == "nd" {
+			fmt.Println(value)
+			break
+		}
+		return_list.PushBack(mapreduce.KeyValue{word, "1"})
+	}
+	return return_list
 }
 
 // iterate over list and add values
 func Reduce(key string, values *list.List) string {
+	return fmt.Sprint(values.Len())
 }
 
 // Can be run in 3 ways:
@@ -20,17 +37,17 @@ func Reduce(key string, values *list.List) string {
 // 2) Master (e.g., go run wc.go master x.txt localhost:7777)
 // 3) Worker (e.g., go run wc.go worker localhost:7777 localhost:7778 &)
 func main() {
-  if len(os.Args) != 4 {
-    fmt.Printf("%s: see usage comments in file\n", os.Args[0])
-  } else if os.Args[1] == "master" {
-    if os.Args[3] == "sequential" {
-      mapreduce.RunSingle(5, 3, os.Args[2], Map, Reduce)
-    } else {
-      mr := mapreduce.MakeMapReduce(5, 3, os.Args[2], os.Args[3])    
-      // Wait until MR is done
-      <- mr.DoneChannel
-    }
-  } else {
-    mapreduce.RunWorker(os.Args[2], os.Args[3], Map, Reduce, 100)
-  }
+	if len(os.Args) != 4 {
+		fmt.Printf("%s: see usage comments in file\n", os.Args[0])
+	} else if os.Args[1] == "master" {
+		if os.Args[3] == "sequential" {
+			mapreduce.RunSingle(5, 3, os.Args[2], Map, Reduce)
+		} else {
+			mr := mapreduce.MakeMapReduce(5, 3, os.Args[2], os.Args[3])
+			// Wait until MR is done
+			<-mr.DoneChannel
+		}
+	} else {
+		mapreduce.RunWorker(os.Args[2], os.Args[3], Map, Reduce, 100)
+	}
 }
--- 6.824-golabs-2014/src/mapreduce/master.go
+++ 6.824-golabs-2014/src/mapreduce/master.go
@@ -1,32 +1,91 @@
 package mapreduce
+
 import "container/list"
 import "fmt"
+import "strconv"
+import "time"
 
 type WorkerInfo struct {
-  address string
-  // You can add definitions here.
+	address string
 }
-
 
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
 // the number of jobs each work has performed.
 func (mr *MapReduce) KillWorkers() *list.List {
-  l := list.New()
-  for _, w := range mr.Workers {
-    DPrintf("DoWork: shutdown %s\n", w.address)
-    args := &ShutdownArgs{}
-    var reply ShutdownReply;
-    ok := call(w.address, "Worker.Shutdown", args, &reply)
-    if ok == false {
-      fmt.Printf("DoWork: RPC %s shutdown error\n", w.address)
-    } else {
-      l.PushBack(reply.Njobs)
-    }
-  }
-  return l
+	l := list.New()
+	for _, w := range mr.Workers {
+		DPrintf("DoWork: shutdown %s\n", w.address)
+		args := &ShutdownArgs{}
+		var reply ShutdownReply
+		ok := call(w.address, "Worker.Shutdown", args, &reply)
+		if ok == false {
+			fmt.Printf("DoWork: RPC %s shutdown error\n", w.address)
+		} else {
+			l.PushBack(reply.Njobs)
+		}
+	}
+	return l
+}
+
+func requestWorker(mr *MapReduce, args *DoJobArgs, channel *chan int) {
+	workerId := 0
+	for {
+		w := mr.Workers[strconv.Itoa(workerId)]
+		var reply DoJobReply
+		ok := call(w.address, "Worker.DoJob", args, &reply)
+		if reply.OK && ok {
+			*channel <- args.JobNumber
+			break
+		} else {
+			workerId = (workerId + 1) % mr.nWorker
+		}
+	}
 }
 
 func (mr *MapReduce) RunMaster() *list.List {
-  // Your code here
-  return mr.KillWorkers()
+	// Your code here
+	// Every mapper has been registered.
+	finishedMapper := make(chan int)
+	finishedRecuder := make(chan int)
+	go func() {
+		for workerName := range mr.registerChannel {
+			mr.Workers[strconv.Itoa(mr.nWorker)] = &WorkerInfo{workerName}
+			mr.nWorker++
+		}
+	}()
+
+	// Wait until at least one worker.
+	for mr.nWorker == 0 {
+		time.Sleep(time.Microsecond * 50)
+	}
+
+	// Send mapper job request to worker.
+	for i := 0; i < mr.nMap; i++ {
+		args := &DoJobArgs{}
+		args.File = mr.file
+		args.JobNumber = i
+		args.Operation = Map
+		args.NumOtherPhase = mr.nReduce
+
+		go requestWorker(mr, args, &finishedMapper)
+	}
+	// Wait for all mappers to finish.
+	for i := 0; i < mr.nMap; i++ {
+		<-finishedMapper
+	}
+
+	// Send reducer job request to worker.
+	for i := 0; i < mr.nReduce; i++ {
+		args := &DoJobArgs{}
+		args.File = mr.file
+		args.JobNumber = i
+		args.Operation = Reduce
+		args.NumOtherPhase = mr.nMap
+		go requestWorker(mr, args, &finishedRecuder)
+	}
+	// Wait for all reducers to finish.
+	for i := 0; i < mr.nReduce; i++ {
+		<-finishedRecuder
+	}
+	return mr.KillWorkers()
 }
